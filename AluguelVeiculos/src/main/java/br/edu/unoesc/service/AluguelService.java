7ac24aff2224f3f764f035d97bcaff1fe2a8ed0d
TheylorMarmitt <TheylorMarmitt@users.noreply.github.com>
2019-05-23 14:21:43 -0300

    mudanças tela de aluguel

---
+++ AluguelVeiculos/src/main/java/br/edu/unoesc/service/AluguelService.java
@@ -0,0 +1,21 @@
+package br.edu.unoesc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.unoesc.dao.AluguelDao;
+import br.edu.unoesc.model.Aluguel;
+
+@Service
+public class AluguelService {
+
+	@Autowired
+	AluguelDao aluguelDao;
+	
+	public void adiciona(Aluguel aluguel) {
+		if(!aluguel.isAtivo()) {
+			aluguel.setAtivo(true);
+		}
+		this.aluguelDao.save(aluguel);
+	}
+}
